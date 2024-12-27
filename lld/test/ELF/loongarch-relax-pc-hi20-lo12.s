03e9596f6253fbd0e66c3ea19dd74cd4f2ff8a8f
yangzhaoxin <yangzhaoxin@loongson.cn>
2024-12-27 17:33:47 +0800

    Add test for PCHi20Lo12

---
+++ lld/test/ELF/loongarch-relax-pc-hi20-lo12.s
@@ -0,0 +1,50 @@
+# REQUIRES: loongarch
+
+# RUN: llvm-mc --filetype=obj --triple=loongarch32 -mattr=+relax %s -o %t.32.o
+# RUN: llvm-mc --filetype=obj --triple=loongarch64 -mattr=+relax %s -o %t.64.o
+
+# RUN: ld.lld --section-start=.text=0x10000 --section-start=.data=0x14000 %t.32.o -o %t.32
+# RUN: ld.lld --section-start=.text=0x10000 --section-start=.data=0x14000 %t.64.o -o %t.64
+# RUN: llvm-objdump -td --no-show-raw-insn %t.32 | FileCheck --check-prefixes=RELAX %s
+# RUN: llvm-objdump -td --no-show-raw-insn %t.64 | FileCheck --check-prefixes=RELAX %s
+
+# RUN: ld.lld --section-start=.text=0x10000 --section-start=.data=0x14000 %t.32.o -shared -o %t.32s
+# RUN: ld.lld --section-start=.text=0x10000 --section-start=.data=0x14000 %t.64.o -shared -o %t.64s
+# RUN: llvm-objdump -td --no-show-raw-insn %t.32s | FileCheck --check-prefixes=RELAX %s
+# RUN: llvm-objdump -td --no-show-raw-insn %t.64s | FileCheck --check-prefixes=RELAX %s
+
+# RUN: ld.lld --section-start=.text=0x10000 --section-start=.data=0x410000 %t.32.o -o %t.32o
+# RUN: ld.lld --section-start=.text=0x10000 --section-start=.data=0x410000 %t.64.o -o %t.64o
+# RUN: llvm-objdump -td --no-show-raw-insn %t.32o | FileCheck --check-prefixes=NORELAX %s
+# RUN: llvm-objdump -td --no-show-raw-insn %t.64o | FileCheck --check-prefixes=NORELAX %s
+
+# RELAX-LABEL: <_start>:
+## offset = 0x14000 - 0x10000 = 4096<<2
+# RELAX-NEXT:      10000:  pcaddi $a0, 4096
+# RELAX-NEXT:              pcaddi $a0, 4095
+# RELAX-NEXT:              pcaddi $a0, 4094
+# RELAX-NEXT:              pcaddi $a0, 4093
+
+# NORELAX-LABEL: <_start>:
+## offset exceed range of pcaddi
+## offset = 0x410000 - 0x10000: 0x400 pages, page offset 0
+# NORELAX-NEXT:    10000:  pcalau12i     $a0, 1024
+# NORELAX-NEXT:            addi.{{[dw]}} $a0, $a0, 0
+# NORELAX-NEXT:            pcalau12i     $a0, 1024
+# NORELAX-NEXT:            ld.{{[dw]}}   $a0, $a0, {{[84]}}
+# NORELAX-NEXT:            pcalau12i     $a0, 1024
+# NORELAX-NEXT:            addi.{{[dw]}} $a0, $a0, 0
+# NORELAX-NEXT:            pcalau12i     $a0, 1024
+# NORELAX-NEXT:            ld.{{[dw]}}   $a0, $a0, {{[84]}}
+
+.section .text
+.global _start
+_start:
+  la.local  $a0, sym
+  la.global $a0, sym
+  la.pcrel  $a0, sym
+  la.got    $a0, sym
+
+.section .data
+sym:
+  .zero 4
